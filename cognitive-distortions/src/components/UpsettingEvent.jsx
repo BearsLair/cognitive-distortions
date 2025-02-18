73cfb7d6fa9f478621bc6f6d14c360e3725a982c
Patrick Gross <bears_lair@protonmail.com>
2025-02-17 18:30:32 -0800

    put upsetting event in its own component

---
+++ cognitive-distortions/src/components/UpsettingEvent.jsx
@@ -0,0 +1,13 @@
+const UpsettingEvent = () => {
+  return (
+    <>
+      <div>
+        <h3>What is event, future or past, that is currently bothering you?</h3>
+        <input id="upsetting-event" type="text" />
+      </div>
+      <hr />
+    </>
+  );
+};
+
+export default UpsettingEvent;
