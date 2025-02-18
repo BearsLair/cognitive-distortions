d7844224c3693bef435057eaad909035df0d2439
Patrick Gross <bears_lair@protonmail.com>
2025-02-17 18:31:51 -0800

    placed components into form div

--- cognitive-distortions/src/App.jsx
+++ cognitive-distortions/src/App.jsx
@@ -1,4 +1,6 @@
 import "./App.css";
+import Emotions from "./components/Emotions";
+import UpsettingEvent from "./components/UpsettingEvent";
 
 function App() {
   return (
@@ -6,6 +8,11 @@ function App() {
       <div>
         <h1 className="title">Cognitive Behavior Therapy Workshop</h1>
       </div>
+      <div className="form-section">
+        <UpsettingEvent />
+        <Emotions />
+      </div>
+      <div className="footer"></div>
     </>
   );
 }
